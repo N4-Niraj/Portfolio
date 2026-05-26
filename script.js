c582538095035417a8aff566700ba914c55044f5
Niraj <bhusalniraj03@gmail.com>
2026-05-26 22:06:42 +0545

    project and skills section improved

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
-    document.querySelector(this.getAttribute("href"))
-      .scrollIntoView({ behavior: "smooth" });
+
+    const target = document.querySelector(this.getAttribute("href"));
+
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    }
   });
 });
